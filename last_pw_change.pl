338a02c2485a920fff5b86fae8bfd408354d2e6d
Jozef Reisinger <jozef.reisinger@gmail.com>
2012-07-24 13:39:26 +0200

    last_pw_change.pl: I can get password change time for all/selected users

--- last_pw_change.pl
+++ last_pw_change.pl
@@ -1,25 +1,29 @@
 #!/usr/bin/perl
-# Parse log file for last password change.
+# Parse /etc/shadow for last password change.
 use strict;
 use warnings;
 
+# Leave this array empty to get all logins
 my @interesting_logins = qw (
-root
-user1
-user2
+  root
+  user1
+  user2
 );
 
 my %pw_change;
 while (<>) {
-  my($login, undef, $last_pw_change) = split /:/, $_;
-  ## Comment out next line for all logins
-  next unless ( grep $login eq $_, @interesting_logins );
-  $last_pw_change = 0 if $last_pw_change eq "";
-  $pw_change{$login} = time()/86400 - $last_pw_change;  # days ago
-  # next comes here ##
+    my ( $login, undef, $last_pw_change ) = split /:/, $_;
+    if (@interesting_logins) {
+        next unless ( grep $login eq $_, @interesting_logins );
+    }
+    $last_pw_change = 0 if $last_pw_change eq "";
+    $pw_change{$login} = time() / 86400 - $last_pw_change;  # days ago
+    # next comes here ##
 }
 
-for my $login ( sort { $pw_change{$b} <=> $pw_change{$a} } keys %pw_change ) {  # sort values by_numbers, descending
-  printf "%-12s %5.0f days (cca. %02.1f years) ago\n",
-    $login, $pw_change{$login}, $pw_change{$login}/365;
+# sort values by numbers, descending
+for my $login ( sort { $pw_change{$b} <=> $pw_change{$a} } keys %pw_change )
+{
+    printf "%-12s %5.0f days (cca. %02.1f years) ago\n",
+      $login, $pw_change{$login}, $pw_change{$login} / 365;
 }
