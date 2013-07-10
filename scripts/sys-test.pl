4a18d25885d4c963e5e89c2e28a071d9b9193fa1
Jozef Reisinger <jozef.reisinger@gmail.com>
2013-07-10 22:16:56 +0200

    Add group check

--- scripts/sys-test.pl
+++ scripts/sys-test.pl
@@ -1,6 +1,8 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+
+#use diagnostics;
 use Test::More 'no_plan';
 
 ## Configuration
@@ -9,16 +11,19 @@ my @files = (
     {
         name  => '/etc/passwd',
         owner => 'root',
+        group => 'root',
         perms => '0644',
     },
     {
         name  => '/etc/group',
         owner => 'root',
+        group => 'root',
         perms => '0644',
     },
     {
         name  => '/etc/shadow',
         owner => 'root',
+        group => 'shadow',
         perms => '0640',
     },
 );
@@ -30,20 +35,32 @@ for my $file (@files) {
     my $name  = $file->{name};
     my $perms = $file->{perms};
     my $owner = $file->{owner};
+    my $group = $file->{group};
     is( access_rights($name), $perms, "perms of $name" );
     is( owner($name),         $owner, "owner of $name" );
+    is( group($name),         $group, "group of $name" );
 }
 
 sub owner {
     my $filename = shift;
-    my $uid      = ( stat($filename) )[4];
-    my $owner    = getpwuid($uid);
+    return unless -e $filename;
+    my ( $uid, $gid ) = ( stat($filename) )[ 4, 5 ];
+    my $owner = getpwuid($uid);
     return $owner;
+}
+
+sub group {
+    my $filename = shift;
+    return unless -e $filename;
+    my ( $uid, $gid ) = ( stat($filename) )[ 4, 5 ];
+    my $group = getgrgid($gid);
+    return $group;
 }
 
 sub access_rights {
     my $filename = shift;
-    my $mode     = ( stat($filename) )[2];
-    my $perms    = sprintf "%04o", $mode & 07777;    # FIXME: undestand this
+    return unless -e $filename;
+    my $mode = ( stat($filename) )[2];
+    my $perms = sprintf "%04o", $mode & 07777;    # discard file type info
     return $perms;
 }
